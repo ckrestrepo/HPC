4d5b968a083fe3e57a58d00204868cc931b6ab9a
ckrestrepo <ckrestrepo>
2015-10-02 13:27:18 -0500

    Modificado los vectores

--- Vectores.c
+++ Vectores.c
@@ -3,22 +3,22 @@
 #include <cuda.h>
 #include <cstdlib>
 #include <time.h>
-#define TAM 30
+#define TAM 1024
 #define blockSize 1024
 
 
-__global__ void vecAdd(int *A, int *B, int *C, int n)
+__global__ void vecAdd(int *A, int *B, int *C)
 {
 	int i = blockIdx.x*blockDim.x+threadIdx.x;
-	if(i < n)
+	if(i < TAM)
 	{
 		C[i] = A[i] + B[i];
 	}
 }
 
-int vectorAdd(int *A, int *B, int *C, int n)
+int vectorAdd(int *A, int *B, int *C)
 {
-	int size = n*sizeof(int);
+	int size = TAM*sizeof(int);
 	int *d_A, *d_B, *d_C;
 	cudaMalloc((void **)&d_A, size);
 	cudaMalloc((void **)&d_B, size);
@@ -32,68 +32,73 @@ int vectorAdd(int *A, int *B, int *C, int n)
   
   	float dimGrid = ceil((float)TAM / (float)blockSize);
 
-	vecAdd<<<dimGrid, n>>>(d_A, d_B, d_C, n);
+	vecAdd<<<dimGrid, TAM>>>(d_A, d_B, d_C);
 	cudaMemcpy(C, d_C, size, cudaMemcpyDeviceToHost);
 
-	for(int i = 0; i < n; i++)
-    {
-	    C[i] = A[i] + B[i];
-	    printf (" %d | ", C[i]);
-    }  
-
   	t2 = clock() - t2;
-  	printf ("\nTiempo GPU: (%f seconds).\n",((float)t2)/CLOCKS_PER_SEC);
+  	printf ("Tiempo GPU: (%f seconds).\n",((float)t2)/CLOCKS_PER_SEC);
 	
 	cudaFree(d_A);
 	cudaFree(d_B);
 	cudaFree(d_C);
-	
 	return 0;
 }
 
-void sumar(int *A, int *B, int *C, int n)
+void sumarSecuencial(int *A, int *B, int *C)
 {
-   
     clock_t t;
     t = clock();
-    for(int i = 0; i < n; i++)
+    for(int i = 0; i < TAM; i++)
     {
 	    C[i]= A[i] + B[i];
-	    printf (" %d | ", C[i]);
     }
    	t = clock() - t;
-   	printf ("\nTiempo CPU: (%f seconds).\n",((float)t)/CLOCKS_PER_SEC);
+   	printf ("Tiempo CPU: (%f seconds).\n",((float)t)/CLOCKS_PER_SEC);
  }
+
+ void agregarDatos (int *Matrix)
+{
+	for(int i = 0; i < TAM; i++)
+	{
+		Matrix[i] = rand() % 5;
+	}
+}
+
+int compararVector(int *M_A, int *M_B)
+{
+	for (int i = 0; i < TAM; ++i)
+	{
+		if (M_A[i] != M_B[i])
+		{
+			printf("Los Vectores son diferentes\n");
+			return 0;
+		}
+	}
+	printf("\nLos Vectores son iguales\n");
+	return 0;
+}
     
 
 int main()
 {
-	int n; //longitud del vector
 	int * A;
 	int * B;
-	int * C;
-  	n = TAM;
+	int * CS;
+	int * CP;
 
-	A = (int*)malloc( n*sizeof(int) );
-	B = (int*)malloc( n*sizeof(int) );
-	C = (int*)malloc( n*sizeof(int) );
-	printf("...Vector A...\n");
-	for(int i = 0; i < n; i++)
-	{
-		A[i] = rand() % 10 ;
-    	printf("%d | ",A[i]);
-	}
-	printf("\n...Vector B...\n");
-	for (int i = 0; i < n; ++i)
-	{
-		B[i] = rand() % 10;
-		printf("%d | ",B[i]);
-	}
-		
-	//vecAddGPU(A,B,C);
+	A = (int*)malloc(TAM*sizeof(int));
+	B = (int*)malloc(TAM*sizeof(int));
+	CS = (int*)malloc(TAM*sizeof(int));
+	CP = (int*)malloc(TAM*sizeof(int));
+
+	agregarDatos(A);
+	agregarDatos(B);
+
 	printf("\nSuma de vector Paralela\n");
-  	vectorAdd(A,B,C,n);
+  	vectorAdd(A,B,CS);
   	printf("\nSuma de vector Secuencial\n");
-  	sumar(A,B,C,n);
+  	sumarSecuencial(A,B,CP);
+  	compararVector(CS, CP);
+
 	return 0;
 }
