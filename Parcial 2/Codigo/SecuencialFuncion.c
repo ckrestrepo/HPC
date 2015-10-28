30b44793798989fc092e6e6e1bebb5bbd9310a47
ckrestrepo <ckrestrepo>
2015-10-28 12:27:15 -0500

    Parcial 2, falta el pdf esta en doc

---
+++ Parcial 2/Codigo/SecuencialFuncion.c
@@ -0,0 +1,55 @@
+#include<iostream>
+#include<cstdlib>
+#include<cstdlib>
+#include<cuda.h>
+#include<highgui.h>
+#include<cv.h>
+
+using namespace std;
+using namespace cv;
+
+
+int main()
+{
+  clock_t start, finish; 
+  double elapsedSequential;
+
+  Mat image;
+  image = imread("inputs/img1.jpg", 0);   // El cero significa que carga la imagen en escala de grises
+  Size s = image.size();
+  int Row = s.width;
+  int Col = s.height;
+
+  unsigned char *img = (unsigned char*)malloc(sizeof(unsigned char)*Row*Col*image.channels());
+
+  if( !image.data )
+  {
+    cout<<"Problemas cargando la Imagen"<<endl;
+    return -1;
+  }
+
+  img = image.data;
+
+  cout<<"... Secuencial ...\n"<<endl;
+  double promedio = 0, temp = 0;
+  for (int i = 1; i <= 20; ++i)
+  {
+    cout <<"Iteracion numero: " << i <<endl;
+    Mat imageOutput;
+    
+    start = clock();
+    
+    Sobel(image, imageOutput, CV_8UC1, 1, 0, 3, 1, 0, BORDER_DEFAULT);
+    
+    finish = clock();
+
+    imwrite("./outputs/1088273734.png", imageOutput);
+
+    elapsedSequential = (((double) (finish - start)) / CLOCKS_PER_SEC );
+    cout<< "El proceso secuencial tomo: " << elapsedSequential << " en ejecutar\n "<< endl;
+    temp = temp + elapsedSequential;
+  }
+  promedio = temp / 20;
+  cout <<"El promedio de tiempo es de: " <<promedio << endl;
+  return 0;
+}
